d09955831125022421cefb0169222fd79a66e2e6
Lizan7 <lizanstha777@gmail.com>
2024-07-20 23:18:26 +0545

    About us page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Link } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import { Lin, BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "../pages/Home"
+import Login from "../pages/Login" 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Home/>
-    </>
+    <div className='App'>
+      <BrowseRouter>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/home" element={<Home/>} />
+        </Routes>
+      </BrowseRouter>
+ 
+    </div>
   )
 }
 
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>Here you can view about the various details of the company</h1>
+    </div>
+  )
+}
+
+export default AboutUs
