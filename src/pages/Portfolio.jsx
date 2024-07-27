c75adea85ea3b39f9cbedefae2104c44857c244f
Lizan7 <lizanstha777@gmail.com>
2024-07-27 21:41:26 +0545

    something

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div className=''>
-      Lizan Shrestha
+    <div className='flex h-screen w-screen justify-center items-center gap-4'>
+      <h1 className='text-2xl font-semibold text-blue-700'>Lizan Shrestha</h1>
+      <p className='flex justify-center items-center'>Welcome to my portfolio</p>
     </div>
   )
 }
